33131736d4aeddf4b685bdd795054e26d0cdfe8e
samuelznewton <57228129+samuelznewton@users.noreply.github.com>
2020-10-28 01:30:09 -0500

    Add files via upload

---
+++ content/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import About_Pages, File, Announcement, Officer, Conference, ImageContent
+# Register your models here.
+admin.site.register(About_Pages)
+admin.site.register(File)
+admin.site.register(Announcement)
+admin.site.register(Officer)
+admin.site.register(Conference)
+admin.site.register(ImageContent)
